1cd4f28cb0c54e1667bee5195569d7a5f34c99a0
TonzToni <98320002+TonzToni@users.noreply.github.com>
2025-06-14 16:00:34 +0100

    added garnet items

--- src/main/java/net/tonz/junker/JunkerClient.java
+++ src/main/java/net/tonz/junker/JunkerClient.java
@@ -1,11 +1,12 @@
 package net.tonz.junker;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.tonz.junker.item.ModItems;
 
 public class JunkerClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-
+        ModItems.registerModItems();
     }
 }
