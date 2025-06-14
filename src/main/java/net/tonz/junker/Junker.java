bef605faccc11cb8e0435074e4af6eb5620acaa5
TonzToni <98320002+TonzToni@users.noreply.github.com>
2025-06-14 15:01:14 +0100

    Initial commit

---
+++ src/main/java/net/tonz/junker/Junker.java
@@ -0,0 +1,16 @@
+package net.tonz.junker;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Junker implements ModInitializer {
+	public static final String MOD_ID = "junker";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/tonz/junker/JunkerClient.java
@@ -0,0 +1,11 @@
+package net.tonz.junker;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JunkerClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
